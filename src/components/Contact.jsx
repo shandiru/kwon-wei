e5de10cb50c49f690f909a5fc8d7681b039e45c0
shandiru <143399530+shandiru@users.noreply.github.com>
2025-12-05 20:46:10 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -87,6 +87,7 @@ export default function Contact() {
                 Reserve Now
               </button>
             </div>
+            <p class="text-xs text-center text-white" >By submitting this form, you agree to us processing your details to respond to your enquiry. Your information is handled securely and in line with our Privacy Policy.</p>
           </form>
         </div>
       </div>
