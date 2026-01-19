c5830e8ce1ea07a60db39081d49febe6add12b1a
shandiru <msddiru@gmail.com>
2026-01-19 19:26:23 +0530

    added

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     return (
         <div>
             <Hero />
-            <Menu />
+            {/* <Menu /> */}
             <About />
             <ImageSlider />
             <Review />
